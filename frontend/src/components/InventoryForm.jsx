950e9e43bf3203524ce56686636a6262ea164076
Aditya <aditya007r29@gmail.com>
2025-03-24 15:08:09 +0530

    A

---
+++ frontend/src/components/InventoryForm.jsx
@@ -0,0 +1,14 @@
+// src/components/InventoryForm.jsx
+import React from 'react';
+
+
+function InventoryForm() {
+
+    return (
+        <>
+            {/* Inventory form */}
+        </>
+    );
+}
+
+export default InventoryForm;
